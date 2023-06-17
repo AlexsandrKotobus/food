0a7d5203549246ba28ab829ecf6c5faf01546cc6
Julia <alexsandr.kotobus@gmail.com>
2023-06-17 21:09:31 +0300

    Fix Issue #{number}: {description}
    R+: {reviewer}

--- js/script.js
+++ js/script.js
@@ -652,8 +652,86 @@ function currentSl(slLenght, cur, slIn){
 
 }
         
+// * * * * * * * * 
+// К А Л Ь К У Л Я Т О Р 
+const result = document.querySelector('.calculating__result span'); //получаем место где будет записан результат
+//переменные для хранения данных
+let sex = 'female', //дефолтное значение - женщина
+ height, weight, age, 
+ ratio = 1.375; //дефолтное значение - коэффициент невысокой активности 
 
+function calcTotal(){
+    //проверка введенных данных - если хотябы одного из этих значений нет
+    if(!sex || !height || !weight || !age || !ratio){
+        result.textContent ="мало данных";
+        return; //выход из калькулятора
+    }
+    //если данные есть - 
+    //расчет для женщин и мужчин
+    if(sex === 'female'){
+        result.textContent = Math.round((447.6 + (9.2 *  weight) + (3.1 * height) - (4.3 * age)) * ratio);
+    } else{
+        result.textContent =  Math.round((88.36 + (13.4 *  weight) + (4.8 * height) - (5.7 * age)) * ratio);
+    }
 
+}
+
+calcTotal();
+//получение данных элементов
+function getStaticInformation(parentSelector, activeClass){
+    //получаем элементы внутри родительского блока - 
+    const elements = document.querySelectorAll(`${parentSelector} div`); //дивы в родительском блоке
+
+//перебираем элементы //раньше было делегирование событий - но ему мешало межблоковые промежутки
+    elements.forEach(elem =>{
+        elem.addEventListener('click', (e) =>{
+            //если у блока есть атрибут data-ratio - то берем его значение, если такого атрибута нет - берем id
+            //e.target - объект события 
+            if(e.target.getAttribute('data-ratio')){ //если атрибут есть
+                ratio = +e.target.getAttribute('data-ratio'); //мы получаем его значение
+            } else {
+                sex = e.target.getAttribute('id'); //если нет атрибута берем id
+            }
+            console.log(ratio, sex); //для проверки
+            //работа с классами активности
+            elements.forEach(elem =>{ //сначала перебираем все 
+                elem.classList.remove(activeClass); //и удаляем у всех элементов класс активности
+            });
+            e.target.classList.add(activeClass); //добавляем класс активности объекту событий
+            calcTotal(); //вызов главной функции для пересчета
+        });
+    });
+}
+//вызов метода выбора с 2ми разными (строка 1 и строка 3) родительскими блоками
+getStaticInformation('#gender', 'calculating__choose-item_active');
+getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+//функция по получению данных с инпута
+function getDinamucInformation(selector){
+    const input = document.querySelector(selector); //получаем инпут
+    //событие ввода данных
+    input.addEventListener('input', () =>{
+        //надо сделать проверку - какому атрибуту соответствует инпут, такое значение и присвоить.
+        switch(input.getAttribute('id')){
+            case 'height':
+                height = +input.value;
+                break;
+            case 'weight' :
+                weight = +input.value;
+                break;
+            case 'age' :
+                age = +input.value;
+                break;
+        }
+        calcTotal(); //вызов главной функции для прересчета
+    });
+    
+}
+// вызываем функцию с тремя разными селекторами
+
+getDinamucInformation('#height');
+getDinamucInformation('#weight');
+getDinamucInformation('#age');
 
 
 
